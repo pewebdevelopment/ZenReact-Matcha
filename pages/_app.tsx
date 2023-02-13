058fc3b9c1e8c877e1d1464e05b42fe0536f0359
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-13 10:20:23 +0530

    "baseUrl": ".",     "paths": {     "@/components/*" : ["components/*"]     },

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { AppProps } from "next/app";
-import Auth from "../components/auth";
+import { UserProvider } from "@/components/auth";
 // import { UserProvider } from "@/components/auth";
 import "../styles/index.css";
 import { QueryClientProvider, QueryClient } from "react-query";
-import { SocketsProvider } from "@/components/Sockets";
+import { SocketsProvider } from "../components/Sockets";
 
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Auth.UserProvider>
+  <UserProvider>
     <QueryClientProvider client={queryClient}>
       <SocketsProvider>
         <Component {...pageProps} />
       </SocketsProvider>
     </QueryClientProvider>
-  </Auth.UserProvider>
+  </UserProvider>
 );
 
 export default App;
